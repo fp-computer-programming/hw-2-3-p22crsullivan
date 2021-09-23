2bbae5dcd182b1c3193c98d20ef229e4f47bd2e0
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-09-23 10:16:43 -0400

    Submit HW 2-3-4

--- hw2-3-4.py
+++ hw2-3-4.py
@@ -1,1 +1,7 @@
 # Author: CRS 9/21/21
+currentStatesPop = 332774929
+ratePerMin = 60 / 25
+ratePerHour = ratePerMin * 60
+ratePerDay = ratePerHour * 24
+popTomorrow = currentStatesPop + ratePerDay
+print(popTomorrow)
